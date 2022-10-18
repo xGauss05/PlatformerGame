8ddfc07ca8759f0fc7c70c6c1321f0f534e051aa
Ethanm-0371 <99729919+Ethanm-0371@users.noreply.github.com>
2022-10-18 12:48:26 +0200

    Added map files

---
+++ Output/Assets/Maps/Base.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Base" tilewidth="128" tileheight="128" spacing="1" margin="1" tilecount="20" columns="5">
+ <image source="freetileset/spritesheet.png" width="646" height="517"/>
+ <terraintypes>
+  <terrain name="Base Terrain" tile="-1"/>
+  <terrain name="Water" tile="-1"/>
+ </terraintypes>
+ <tile id="1" terrain=",,,0"/>
+ <tile id="2" terrain=",,0,0"/>
+ <tile id="3" terrain=",,0,"/>
+ <tile id="4" terrain=",,1,1"/>
+ <tile id="6" terrain=",0,,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,,0,"/>
+ <tile id="9" terrain="1,1,1,1"/>
+ <tile id="12" terrain="0,0,,"/>
+ <tile id="15" terrain=",0,,"/>
+ <tile id="19" terrain="0,,,"/>
+</tileset>
